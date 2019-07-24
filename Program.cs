4fe14528e80af628a969c6d1797ed07bce471262
Zach Selindh <zachselindh@gmail.com>
2019-07-24 18:54:23 -0500

    Lerning doubles.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace helloworld
             int num2 = 5;
             int sum = num1 + num2;
 
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 + d2;
+
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
-            Console.Read();
+            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumD);
         }
     }
 }
