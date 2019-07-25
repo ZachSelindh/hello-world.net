3eb850fd78712d64bb7185fa2ba1a706002cb97f
Zach Selindh <zachselindh@gmail.com>
2019-07-24 19:31:29 -0500

    Learning Int32 parsing.

--- Program.cs
+++ Program.cs
@@ -6,16 +6,13 @@ namespace helloworld
     {
         static void Main(string[] args)
         {
-            int num1 = 13;
-            int num2 = 5;
-            int sum = num1 + num2;
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+            int result = num1 + num2;
 
-            double d1 = 3.5;
-            double d2 = 1.337;
-            double sumD = d1 + d2;
-
-            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
-            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sumD);
+            Console.WriteLine(result);
         }
     }
 }
